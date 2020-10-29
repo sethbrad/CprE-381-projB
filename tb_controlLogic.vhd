ab207d423bd25d4c4af263008fc86400c0e7665b
sethbrad <sethbrad@iastate.edu>
2020-10-29 13:10:05 -0500

    Ready to test control logic

--- tb_controlLogic.vhd
+++ tb_controlLogic.vhd
@@ -2,21 +2,37 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity tb_controlLogic is 
-    port(opcode: in std_logic_vector(5 downto 0));
+    port(opcode : in std_logic_vector(5 downto 0);
+         funct : in std_logic_vector(5 downto 0));
 end tb_controlLogic;
 
 architecture struct of controlLogic is
 
 component controlLogic is 
     port(opcode : in std_logic_vector(5 downto 0);
+	     funct : in std_logic_vector(5 downto 0);
              memRead : out std_logic;
              memToReg : out std_logic;
              ALUOp : out std_logic;
              memWrie : out std_logic;
              ALUSrc : out std_logic;
-             RegWrite : out std_logic);
+             regWrite : out std_logic);
 end component;
 
+signal opcodeSig, functSig : std_logic_vector(5 downto 0);
+signal memReadSig, memToRegSig, ALUOpSig, memWriteSig, ALUSrcSig, regWriteSig : std_logic;
+
+begin
+DUT: controlLogic port map(opcodeSig, functSig, memReadSig, memToRegSig, ALUOpSig, memWriteSig, ALUSrcSig, RegWriteSig);
+
+test: process
 begin
 
+opcodeSig <= "000000";
+functSig <= "100000";
+wait for 100 ns;
+
+wait;
+
+end process;
 end struct;
